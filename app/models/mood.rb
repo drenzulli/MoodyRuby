3aa66294e883e74205691e676b33275b91b13f34
drenzulli <dylanr@sas.upenn.edu>
2014-04-06 20:28:06 -0400

    Now comes with validations for moods

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,3 +1,8 @@
 class Mood < ActiveRecord::Base
   belongs_to :user
+
+  moodFormatValidation = /\D+/
+  
+  validates :feeling, length: {within: 3..20}, presence: true, format: {with: moodFormatValidation}, uniqueness: true
+  validates :adjective, length: {within: 3..20}, presence: true, format: {with: moodFormatValidation}
 end
