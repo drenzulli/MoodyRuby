dd19858386cfded638ce495a89131e4a3e4fe68f
drenzulli <dylanr@sas.upenn.edu>
2014-04-06 23:56:24 -0400

    Styled main navbar slightly, also added a welcome page (login currently required).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 MoodyRuby::Application.routes.draw do
+  get "home/index"
   devise_for :users
   resources :users
   resources :moods
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
