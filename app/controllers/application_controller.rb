6d89f34d7f15d099e78ebe20af4dc2e7b4204afc
drenzulli <dylan.renzulli@gmail.com>
2014-05-04 16:01:21 -0400

    Added Phonenumber field to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :phonenumber, :password, :password_confirmation) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :phonenumber, :password, :password_confirmation, :current_password) }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428161911) do
+ActiveRecord::Schema.define(version: 20140504193625) do
 
   create_table "moods", force: true do |t|
     t.string   "feeling"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140428161911) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "phonenumber"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
