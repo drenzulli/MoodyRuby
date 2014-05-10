e57feb8ace95c18945e9cc5ad7e77fa4d2e9bb8d
drenzulli <dylan.renzulli@gmail.com>
2014-05-09 22:47:12 -0400

    Updated readme and phonenumber validation

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -2,8 +2,9 @@ class Mood < ActiveRecord::Base
   belongs_to :user
 
   moodFormatValidation = /\D+/
+  phoneValidation 	   = /\d/
   
   validates :feeling, length: {within: 3..20}, presence: true, format: {with: moodFormatValidation}, uniqueness: true
   validates :adjective, presence: true, format: {with: moodFormatValidation}
-  validates :phonenumber, presence: true
+  validates :phonenumber, presence: true, length: {within: 10..11}, format: {with: phoneValidation}
 end
